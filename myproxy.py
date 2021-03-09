4b94b85b5e17a2d0b9db2387f9751983b301a3b2
olyanogovitsyna <oliyangvts@mail.ru>
2021-03-10 01:06:11 +0900

    lab1

--- myproxy.py
+++ myproxy.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello_world():
-msg="Hello IVT-17!"
-if request.args.get('url'):
-try:
-r = requests.get(request.args['url'])
-if r.status_code==200:
-return r.content
-except:
-msg = f"Не удалось загрузить {request.args.get('url')}"
-return '<h1>' + msg + '''</h1>
-<form method="get">
-<input type="text" name="url">
-<input type="submit">
-</form>
-'''
+    msg="Hello IVT-17!"
+    if request.args.get('url'):
+        try:
+            r = requests.get(request.args['url'])
+            if r.status_code==200:
+                return r.content
+        except:
+            msg = f"Не удалось загрузить {request.args.get('url')}" 
+    return '<h1>' + msg + '''</h1>
+    <form method="get">
+    <input type="text" name="url">
+    <input type="submit">
+    </form>
+    '''
 
 if __name__ == '__main__':
-app.run()
+    app.run()
